8ae851c883033a0de850d39f3bb6185be45756d7
tyapca7 <tyapca7@gmail.com>
2012-02-14 13:21:02 +0100

    Chao-Jaccard and Chao-Sorensen added

--- eco/sim/chao.go
+++ eco/sim/chao.go
@@ -3,7 +3,6 @@
 // account the number of unseen shared species using Chao's method for
 // estimating the number of unseen species.
 // Chao, A., Chazdon, R. L., Colwell, R. K. and Shen, T. (2005). A new statistical approach for assessing similarity of species composition with incidence and abundance data. Ecology Letters 8, 148–159. 
-// Similarity is 1.00-v
 
 package sim
 
@@ -13,6 +12,8 @@ import (
 )
 
 // Chao distance matrix
+// Chao et al. (2005)
+// Algorithm inspired by R:vegan
 func Chao_D(data *Matrix) *Matrix {
 	var v float64
 
@@ -20,6 +21,7 @@ func Chao_D(data *Matrix) *Matrix {
 	cols := data.C
 	out := NewMatrix(rows, rows)
 	// check whether data are integers; if not, truncate them
+	WarnIfNotCounts(data)
 	TruncData(data)
 
 	for i := 0; i < rows; i++ {
@@ -101,6 +103,210 @@ func Chao_D(data *Matrix) *Matrix {
 			}
 			out.Set(i, j, v)
 			out.Set(j, i, v)
+		}
+	}
+	return out
+}
+
+/*
+`chao.jaccard` <-
+function(x,y)
+{ n<-sum(x)
+  m<-sum(y)
+  if (length(x[y>0&x==2])==0) f2plus<-1
+  else f2plus<-length(x[y>0&x==2])
+  p1<-sum(x[y>0]/n)
+  p2<-((m-1)/m)*(length(x[y>0&x==1])/(2*f2plus))
+  p3<-sum(x[y==0]/n)
+  u<-p1+p2*p3
+  if (u>1) u<-1
+  if (length(y[x>0&y==2])==0) fplus2<-1
+  else fplus2<-length(y[x>0&y==2])
+  q1<-sum(y[x>0]/m)
+  q2<-((n-1)/n)*(length(y[x>0&y==1])/(2*fplus2))
+  q3<-sum(y[x==0]/m)
+  v<-q1+q2*q3
+  if (v>1) v<-1
+  if (u==0&v==0) c.j<-0
+  else c.j<-(u*v)/(u+v-(u*v))
+  return(c.j)
+}
+*/
+// Chao - Jaccard similarity matrix
+// Chao’s Jaccard shared species estimators for use with incomplete datasets
+// Chao et al. (2005)
+// Algorithm inspired by R:fossil
+func ChaoJaccard_S(data *Matrix) *Matrix {
+	rows := data.R
+	cols := data.C
+	out := NewMatrix(rows, rows)
+
+	// check whether data are integers; if not, truncate them
+	WarnIfNotCounts(data)
+	TruncData(data)
+	c := 0.0
+	for i := 0; i < rows; i++ {
+		for j := i; j < rows; j++ {
+			sumX := 0.0
+			sumY := 0.0
+			f2plus := 0.0
+			fplus2 := 0.0
+			len1 := 0.0
+			len2 := 0.0
+			p1 := 0.0
+			p2 := 0.0
+			p3 := 0.0
+			q1 := 0.0
+			q2 := 0.0
+			q3 := 0.0
+
+			for k := 0; k < cols; k++ {
+				x := data.Get(i, k)
+				y := data.Get(j, k)
+				sumX += x
+				sumY += y
+				if x == 2 && y > 0 {
+					f2plus++
+				}
+				if x == 1 && y > 0 {
+					len1++
+				}
+				if y > 0 {
+					p1 += x
+				}
+				if y == 0 {
+					p3 += x
+				}
+				if y == 2 && x > 0 {
+					fplus2++
+				}
+				if y == 1 && x > 0 {
+					len2++
+				}
+				if x > 0 {
+					q1 += y
+				}
+				if x == 0 {
+					q3 += y
+				}
+			}
+			if f2plus == 0 {
+				f2plus = 1
+			}
+			if fplus2 == 0 {
+				fplus2 = 1
+			}
+			p1 /= sumX
+			p2 = ((sumY - 1) / sumY) * (len1 / (2 * f2plus))
+			p3 /= sumX
+			u := p1 + p2*p3
+			if u > 1 {
+				u = 1
+			}
+			q1 /= sumY
+			q2 = ((sumX - 1) / sumX) * (len2 / (2 * fplus2))
+			q3 /= sumY
+			v := q1 + q2*q3
+			if v > 1 {
+				v = 1
+			}
+			if u == 0 && v == 0 {
+				c = 0
+			} else {
+				c = (u * v) / (u + v - (u * v))
+			}
+			out.Set(i, j, c)
+			out.Set(j, i, c)
+		}
+	}
+	return out
+}
+
+// Chao - Sorensen similarity matrix
+// Chao’s Sorensen shared species estimators for use with incomplete datasets
+// Chao et al. (2005)
+// Algorithm inspired by R:fossil
+func ChaoSorensen_S(data *Matrix) *Matrix {
+	rows := data.R
+	cols := data.C
+	out := NewMatrix(rows, rows)
+
+	// check whether data are integers; if not, truncate them
+	WarnIfNotCounts(data)
+	TruncData(data)
+	c := 0.0
+	for i := 0; i < rows; i++ {
+		for j := i; j < rows; j++ {
+			sumX := 0.0
+			sumY := 0.0
+			f2plus := 0.0
+			fplus2 := 0.0
+			len1 := 0.0
+			len2 := 0.0
+			p1 := 0.0
+			p2 := 0.0
+			p3 := 0.0
+			q1 := 0.0
+			q2 := 0.0
+			q3 := 0.0
+
+			for k := 0; k < cols; k++ {
+				x := data.Get(i, k)
+				y := data.Get(j, k)
+				sumX += x
+				sumY += y
+				if x == 2 && y > 0 {
+					f2plus++
+				}
+				if x == 1 && y > 0 {
+					len1++
+				}
+				if y > 0 {
+					p1 += x
+				}
+				if y == 0 {
+					p3 += x
+				}
+				if y == 2 && x > 0 {
+					fplus2++
+				}
+				if y == 1 && x > 0 {
+					len2++
+				}
+				if x > 0 {
+					q1 += y
+				}
+				if x == 0 {
+					q3 += y
+				}
+			}
+			if f2plus == 0 {
+				f2plus = 1
+			}
+			if fplus2 == 0 {
+				fplus2 = 1
+			}
+			p1 /= sumX
+			p2 = ((sumY - 1) / sumY) * (len1 / (2 * f2plus))
+			p3 /= sumX
+			u := p1 + p2*p3
+			if u > 1 {
+				u = 1
+			}
+			q1 /= sumY
+			q2 = ((sumX - 1) / sumX) * (len2 / (2 * fplus2))
+			q3 /= sumY
+			v := q1 + q2*q3
+			if v > 1 {
+				v = 1
+			}
+			if u == 0 && v == 0 {
+				c = 0
+			} else {
+				c = (2*u*v)/(u+v)
+			}
+			out.Set(i, j, c)
+			out.Set(j, i, c)
 		}
 	}
 	return out
