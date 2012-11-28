4e7df5f95bb2875282cf58733922858e553d8609
Peter A. Cejchan <tyapca7@gmail.com>
2012-11-28 09:22:54 +0100

    some doc added

--- eco/div/doc.go
+++ eco/div/doc.go
@@ -1,4 +1,4 @@
 // Copyright 2012 The Eco Authors. All rights reserved. See the LICENSE file.
 
 // Package div implements a diversity / equitability (evenness) / inequality library.
-package documentation
+package div
