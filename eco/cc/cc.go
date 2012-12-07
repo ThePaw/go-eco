c94ad98df272552dfe40e84a837ae290b347830f
Peter A. Cejchan <tyapca7@gmail.com>
2012-12-07 13:57:07 +0100

    coenocline heavily reworked; Gauss and beta fused; alpha and beta models added; models reparametrized using tolerance (scale) instead of std.

--- eco/cc/cc.go
+++ eco/cc/cc.go
@@ -3,7 +3,9 @@
 package cc
 
 // Coenocline modelling functions. 
-// To do: model for α, γ 
+// To do: 
+// implement carrying capacity Austin 2006
+// implement species interactions
 
 import (
 	. "code.google.com/p/go-eco/eco/aux"
@@ -17,7 +19,6 @@ import (
 const (
 	flat = iota
 	gaussian
-	beta
 	paretoI
 	paretoII
 	paretoIII
@@ -29,6 +30,55 @@ const (
 	planck
 	zeta
 )
+
+type Models struct {
+	SRF        string  // type of Species response Function
+	Samp       int     // spacing of samples
+	Opt        int     // distribution of optima
+	OptLoc     float64 // its mean
+	OptScale   float64 // and tolerance
+	Max        int     // distribution of maximum values of population densities
+	MaxLoc     float64 // its mean
+	MaxScale   float64 // and tolerance
+	Tol        int     // distribution of species tolerances
+	TolLoc     float64 // its mean
+	TolScale   float64 // and tolerance
+	MaxTolRho  float64 // correlation between population size and tolerance
+	Beta       int     // distribution of alpha and gamma for beta function
+	AlphaLoc   float64 // its mean
+	AlphaScale float64 // and tolerance
+	GammaLoc   float64 // its mean
+	GammaScale float64 // and tolerance
+	Noise      float64 // amount of external noise
+}
+
+// SetUpModels fills in a structure that holds model types and params. 
+func (m *Models) SetUpModels(srfModel string, sampModel, optModel, popModel, tolModel, betaParamModel int, μOpt, εOpt, μMax, εMax, μTol, εTol, ρMaxTol, μα, εα, μγ, εγ, εNoise float64) {
+	// Loc = location, Scale = scale
+	m.SRF = srfModel
+	m.Samp = sampModel
+	m.Opt = optModel
+	m.OptLoc = μOpt
+	m.OptScale = εOpt // relative to 1, the length of the gradient
+	m.Max = popModel
+	m.MaxLoc = μMax
+	m.MaxScale = εMax // relative to MaxLoc
+	m.Tol = tolModel
+	m.TolLoc = μTol   // relative to  1, the length of the gradient
+	m.TolScale = εTol // relative to  TolLoc
+	m.MaxTolRho = ρMaxTol
+	m.Beta = betaParamModel
+	m.AlphaLoc = μα
+	m.AlphaScale = εα
+	m.GammaLoc = μγ
+	m.GammaScale = εγ
+	m.Noise = εNoise
+
+	// because some ε are originally relative numbers, recalculate
+	m.MaxScale *= m.MaxLoc
+	m.TolScale *= m.TolLoc
+	return
+}
 
 // Generates sampling points along the gradient.
 func generate_points(k, spacing int) (pts []float64) {
@@ -57,10 +107,10 @@ func generate_points(k, spacing int) (pts []float64) {
 	return
 }
 
-// Gaussian response function. 
+// GaussSRF  computes the Gaussian response function. 
 func GaussSRF(x float64, par ...float64) float64 {
 	// opt	 	optimum = modus = position of max. population size on the gradient (= mean for Gaussian)
-	// tol		tolerance (=range in Austin, 2006)
+	// tol		tolerance (= range in Austin, 2006)
 	// x		point on the gradient
 	opt, tol := par[0], par[1]
 	σ := tol / 6 // 99.7% of population lies within μ±3*σ for Normal (Gaussian) distribution
@@ -68,25 +118,49 @@ func GaussSRF(x float64, par ...float64) float64 {
 	return NormalPDFAt(opt, σ, x) / y
 }
 
-// GeneralisedBeta response function. 
-// Austin (2006), p. 200. 
-func GenBetaSRF(x float64, par ...float64) float64 {
-	// opt	 	optimum = modus = position of max. population size on the gradient (= mean for Gaussian)
-	// tol		tolerance (=range in Austin, 2006)
+// BetaSRF computes the β-function of Austin, 1976
+func BetaSRF(x float64, par ...float64) (y float64) {
+	// Austin, M. P. (1976). On non-linear species response models in ordination. Vegetatio, 33(1), 33-41.
+	// http://cc.oulu.fi/~jarioksa/softhelp/betasimu.htm
+
+	// opt	optimum = modus = position of max. population size on the gradient (= mean for Gaussian)
+	// tol	tolerance (= range in Austin, 1994)
 	// α, γ	params of β-function
 	// x		point on the gradient
+
 	opt, tol, α, γ := par[0], par[1], par[2], par[3]
-	b := α / (α + γ)
-	d := math.Pow(b, α) * math.Pow(1-b, γ)
-	e := (x-opt)/tol + b
-	a := 1 / d * (math.Pow(e, α) * math.Pow(1-e, γ))
-	return a
+
+	// calculate lower and upper limit from optimum and tolerance
+	// needs to be reimplemented: just a quick hack valid only for symmetric (α = γ) cases
+	lo := opt - 0.5*tol
+	hi := opt + 0.5*tol
+
+	// Return zero if x is not in (lo,hi)
+	if x <= lo || x >= hi {
+		y = 0
+	} else {
+		// Solve k from the maximum height of the response function
+		// thanks to Jari Oksanen, betasimu.c
+		max := 1.0 // desired maximum value
+		tol := hi - lo
+		t4 := tol / (α + γ)
+		t6 := math.Pow(α*t4, α)
+		t11 := math.Pow(γ*t4, γ)
+		k := max / t6 / t11
+		// evaluate the beta-function at x
+		t2 := math.Pow(x-lo, α)
+		t3 := math.Pow(hi-x, γ)
+		y = k * t2 * t3
+	}
+	return
 }
 
-//  External influences modelled as Gaussian "error" (in terms of Fisherian statistics). 
-func noise(μ, σ float64) float64 {
+//  External influences modelled as Gaussian "error". 
+func noise(μ, ε float64) float64 {
 	// μ	 	theoretical value of density according to model
-	// σ		standard deviation due to external (to the model) factors
+	// ε		scale of noise, relative to μ=1
+
+	σ := ε / 6
 	y := NormalNext(μ, σ)
 	if y < 0 {
 		y = 0
@@ -94,6 +168,190 @@ func noise(μ, σ float64) float64 {
 	return y
 }
 
+// Random variable generator for optima, population sizes and tolerances, and for α, γ params of the β-function
+func rndFn(which int, μ, ε float64) func() (x float64) {
+	// μ	mean or modus
+	// ε	tolerance
+
+	return func() (x float64) {
+		switch which {
+		case flat: // Uniform distribution
+			lo := μ - 0.5*ε
+			hi := μ + 0.5*ε
+			x = UniformNext(lo, hi)
+		case gaussian: // Gaussian distribution
+			σ := ε / 6
+			x = NormalNext(μ, σ)
+			/*
+						case levy: // Lévy (unshifted)
+							x = LevyNext(6/ε)	// terrible ad-hockery, empirical
+						case paretoI: // Pareto I
+				minValue := 0.01
+							x = ParetoNext(minValue, 6/ε)	// terrible ad-hockery, empirical
+						case ParetoII: // Pareto II distribution
+							x = ParetoIINext(a, b)
+						case ParetoIII: // Pareto III distribution
+							x = ParetoIIINext(a, b)
+						case ParetoIV: // Pareto IV distribution
+							x = ParetoIVNext(a, b)
+						case ParetoG: // Generalized Pareto distribution
+							x = ParetoGNext(a, b, c)
+						case ParetoTap: // tapered Pareto distribution
+							x = ParetoTapNext(a, b, c)
+						case ParetoSing: // single-parameter Pareto distribution
+							x = ParetoSingNext(a, b)
+						case Yule: // Yule distribution
+							x = YuleNext(a)
+						case Planck: // Planck distribution
+							x = PlanckNext(a, b)
+						case Zeta: // Zeta distribution
+							x = ZetaNext(a)
+			*/
+		}
+		return
+	}
+}
+
+func responseFn(which string) func(x float64, par ...float64) float64 {
+	fn := GaussSRF
+	switch which {
+	case "gauss": // Gaussian distribution
+		fn = GaussSRF
+	case "beta": // β-function of Austin, 1976
+		fn = BetaSRF
+	}
+	return fn
+}
+
+// Coenocline returns a matrix of species responses along (environmental) gradient. 
+func Coenocline(nSpec, nSamp int, m Models) (out *Matrix) {
+	var (
+		opt, tol, aMax float64
+	)
+
+	out = NewMatrix(nSamp, nSpec)
+
+	// if maxima and tolerance models are both Gaussian, allow for covariance:  model is Multivariate Normal
+	mvNorm := false
+	if m.Max == gaussian && m.Tol == gaussian {
+		mvNorm = true
+	}
+
+	srf := responseFn(m.SRF)
+	points := generate_points(nSamp, m.Samp)   // generate sampling points
+	rngO := rndFn(m.Opt, m.OptLoc, m.OptScale) // optima distribution model
+	rngM := rndFn(m.Max, m.MaxLoc, m.MaxScale) // maxima distribution model
+	rngT := rndFn(m.Tol, m.TolLoc, m.TolScale)
+	rngA := rndFn(m.Beta, m.AlphaLoc, m.AlphaScale) // alpha distribution model
+	rngG := rndFn(m.Beta, m.GammaLoc, m.GammaScale) // gamma distribution model
+	α, γ := 0.0, 0.0                                // not used outside beta model, just dummy values
+
+	// for every species: 
+	for j := 0; j < nSpec; j++ {
+
+		// generate optimum (point on the gradient)
+		opt = rngO()
+
+		// generate species' population size and tolerance
+		if mvNorm { // population size and tolerance model is Multivariate Normal
+			// func Zeros(rows, cols int) *DenseMatrix
+			mu := mtx.Zeros(2, 1)
+			mu.Set(0, 0, m.MaxLoc)
+			mu.Set(1, 0, m.TolLoc)
+			cov := mtx.Ones(2, 2)
+			cov.Set(0, 0, m.MaxScale*m.MaxScale) // needs justification
+			cov.Set(0, 1, m.MaxTolRho*m.MaxScale*m.TolScale)
+			cov.Set(1, 0, m.MaxTolRho*m.MaxScale*m.TolScale)
+			cov.Set(1, 1, m.TolScale*m.TolScale)
+			// func MVNormalNext(opt *DenseMatrix, Σ *DenseMatrix) *DenseMatrix
+			mvMat := MVNormalNext(mu, cov)
+			aMax = mvMat.Get(0, 0)
+			tol = mvMat.Get(1, 0)
+		} else {
+			aMax = rngM()
+			tol = rngT() // generate tolerance (range of acceptable gradient values)
+		}
+
+		// force max population density and tolerance within some considerable limits
+		lo := m.MaxLoc - 3*m.MaxScale
+		if lo < 0 {
+			lo = 0
+		}
+		if aMax < lo {
+			aMax = lo
+		}
+		hi := m.MaxLoc + 3*m.MaxScale
+		if aMax > hi {
+			aMax = hi
+		}
+		lo = m.TolLoc - 3*m.TolScale
+		if lo < 0 {
+			lo = 0.1 * m.TolScale
+		}
+		if tol < lo {
+			tol = lo
+		}
+		hi = m.TolLoc + 3*m.TolScale
+		if tol > hi {
+			tol = hi
+		}
+
+		// skew beta towards gradient optimum (Austin 2006): swap α, γ if needed
+		if m.SRF == "beta" {
+			α = rngA()
+			γ = rngG()
+			if m.Opt == gaussian && opt > m.OptLoc {
+				if α > γ {
+					swap := α
+					α = γ
+					γ = swap
+				}
+			} else {
+				if α > γ {
+					swap := α
+					α = γ
+					γ = swap
+				}
+			}
+		}
+
+		for i := 0; i < nSamp; i++ {
+			x := points[i]
+			y := srf(x, opt, tol, α, γ)
+			// add "noise", if required
+			if m.Noise > 0 {
+				y = noise(y, m.Noise)
+			}
+			// scale by max population density
+			y *= aMax
+			out.Set(i, j, y)
+		}
+	}
+	return
+}
+
+/* Something went wrong... bad formula?
+// GeneralisedBeta response function. 
+// Austin et al. (2006), p. 200. 
+func GenBetaSRF(x float64, par ...float64) float64 {
+	// Austin, M. P., Belbin, L., Meyers, J. A., Doherty, M. D., & Luoto, M. (2006). Evaluation of statistical models used for predicting plant species distributions: 
+	// Role of artificial data and theory. Ecological Modelling, 199(2), 197-216. 
+
+	// opt	 	optimum = modus = position of max. population size on the gradient (= mean for Gaussian)
+	// tol		tolerance (=range in Austin, 2006)
+	// α, γ	params of β-function
+	// x		point on the gradient
+
+	opt, tol, α, γ := par[0], par[1], par[2], par[3]
+	b := α / (α + γ)
+	d := math.Pow(b, α) * math.Pow(1-b, γ)
+	e := (x-opt)/tol + b
+	a := (math.Pow(e, α) * math.Pow(1-e, γ))/d 
+	return a
+}
+*/
+
+/* =========================================OLD CODE =========================================
 // Random variable generator for optima, population sizes and tolerances
 func rndFn(which int, μ, σ float64) func() (x float64) {
 	return func() (x float64) {
@@ -106,71 +364,11 @@ func rndFn(which int, μ, σ float64) func() (x float64) {
 		case beta: // Beta
 			a, b := BetaReparamMeanStd(μ, σ)
 			x = BetaNext(a, b)
-			/*
-				case ParetoI: // Pareto I
-					x = ParetoNext(a, b)
-				case ParetoII: // Pareto II distribution
-					x = ParetoIINext(a, b)
-				case ParetoIII: // Pareto III distribution
-					x = ParetoIIINext(a, b)
-				case ParetoIV: // Pareto IV distribution
-					x = ParetoIVNext(a, b)
-				case ParetoG: // Generalized Pareto distribution
-					x = ParetoGNext(a, b, c)
-				case ParetoTap: // tapered Pareto distribution
-					x = ParetoTapNext(a, b, c)
-				case ParetoSing: // single-parameter Pareto distribution
-					x = ParetoSingNext(a, b)
-				case Yule: // Yule distribution
-					x = YuleNext(a)
-				case Planck: // Planck distribution
-					x = PlanckNext(a, b)
-				case Zeta: // Zeta distribution
-					x = ZetaNext(a)
-			*/
 		}
 		return
 	}
 }
 
-type Models struct {
-	SRF       string  // type of Species response Function
-	Samp      int     // spacing of samples
-	Opt       int     // distribution of optima
-	OptMean   float64 // its mean
-	OptSigma  float64 // and variance
-	Pop       int     // distribution of population sizes
-	PopMean   float64 // its mean
-	PopSigma  float64 // and variance
-	Tol       int     // distribution of species tolerances
-	TolMean   float64 // its mean
-	TolSigma  float64 // and variance
-	PopTolRho float64 // correlation between population size and tolerance
-	Noise     float64 // amount of external noise
-}
-
-// SetUpModels fills in a structure that holds model types and params. 
-func (m *Models) SetUpModels(srfModel string, sampModel, optModel, popModel, tolModel int, μOpt, σOpt, μPop, σPop, μTol, σTol, ρPopTol, σNoise float64) {
-	m.SRF = srfModel
-	m.Samp = sampModel
-	m.Opt = optModel
-	m.OptMean = μOpt
-	m.OptSigma = σOpt
-	m.Pop = popModel
-	m.PopMean = μPop
-	m.PopSigma = σPop
-	m.Tol = tolModel
-	m.TolMean = μTol
-	m.TolSigma = σTol
-	m.PopTolRho = ρPopTol
-	m.Noise = σNoise
-
-	m.PopSigma *= m.PopMean // because σPop is originally relative number
-	m.TolSigma *= m.TolMean // same for tolerance
-	return
-}
-
-/*
 // Coenocline returns a matrix of species responses along (environmental) gradient. 
 func Coenocline(nSpec, nSamp int, m Models) (out *Matrix) {
 	var (
@@ -181,16 +379,16 @@ func Coenocline(nSpec, nSamp int, m Models) (out *Matrix) {
 
 	srf := responseFn(m.SRF)
 	points := generate_points(nSamp, m.Samp)    // generate sampling points
-	rngO := rndFn(m.Opt, m.OptMean, m.OptSigma) // optima distribution model
+	rngO := rndFn(m.Opt, m.OptMean, m.OptScale) // optima distribution model
 
 	// if population size and tolerance models are both Gaussian, allow for covariance:  model is Multivariate Normal
 	mvNorm := false
-	if m.Pop == gaussian && m.Tol == gaussian {
+	if m.Max == gaussian && m.Tol == gaussian {
 		mvNorm = true
 	}
 
-	rngA := rndFn(m.Pop, m.PopMean, m.PopSigma) // population size distribution model
-	rngT := rndFn(m.Tol, m.TolMean, m.TolSigma) // tolerance distribution model
+	rngM := rndFn(m.Max, m.MaxLoc, m.MaxScale) // population size distribution model
+	rngT := rndFn(m.Tol, m.TolLoc, m.TolScale) // tolerance distribution model
 
 	// for every species: 
 	for j := 0; j < nSpec; j++ {
@@ -201,42 +399,42 @@ func Coenocline(nSpec, nSamp int, m Models) (out *Matrix) {
 		if mvNorm { // population size and tolerance model is Multivariate Normal
 			// func Zeros(rows, cols int) *DenseMatrix
 			mu := mtx.Zeros(2, 1)
-			mu.Set(0, 0, m.PopMean)
-			mu.Set(1, 0, m.TolMean)
+			mu.Set(0, 0, m.MaxLoc)
+			mu.Set(1, 0, m.TolLoc)
 			cov := mtx.Ones(2, 2)
-			cov.Set(0, 0, m.PopSigma*m.PopSigma) // needs justification
-			cov.Set(0, 1, m.PopTolRho*m.PopSigma*m.TolSigma)
-			cov.Set(1, 0, m.PopTolRho*m.PopSigma*m.TolSigma)
-			cov.Set(1, 1, m.TolSigma*m.TolSigma)
+			cov.Set(0, 0, m.MaxScale*m.MaxScale) // needs justification
+			cov.Set(0, 1, m.MaxTolRho*m.MaxScale*m.TolScale)
+			cov.Set(1, 0, m.MaxTolRho*m.MaxScale*m.TolScale)
+			cov.Set(1, 1, m.TolScale*m.TolScale)
 			// func MVNormalNext(μ *DenseMatrix, Σ *DenseMatrix) *DenseMatrix
 			mvMat := MVNormalNext(mu, cov)
 			popSize = mvMat.Get(0, 0)
 			σ = mvMat.Get(1, 0)
 		} else {
-			popSize = rngA()
+			popSize = rngM()
 			σ = rngT() // generate tolerance (range of acceptable gradient values)
 		}
 
 		// force population size and tolerance within some considerable limits
-		lo := m.PopMean - 3*m.PopSigma
+		lo := m.MaxLoc - 3*m.MaxScale
 		if lo < 0 {
 			lo = 0
 		}
 		if popSize < lo {
 			popSize = lo
 		}
-		hi := m.PopMean + 3*m.PopSigma
+		hi := m.MaxLoc + 3*m.MaxScale
 		if popSize > hi {
 			popSize = hi
 		}
-		lo = m.TolMean - 3*m.TolSigma
+		lo = m.TolLoc - 3*m.TolScale
 		if lo < 0 {
-			lo = 0.1 * m.TolSigma
+			lo = 0.1 * m.TolScale
 		}
 		if σ < lo {
 			σ = lo
 		}
-		hi = m.TolMean + 3*m.TolSigma
+		hi = m.TolLoc + 3*m.TolScale
 		if σ > hi {
 			σ = hi
 		}
@@ -255,99 +453,3 @@ func Coenocline(nSpec, nSamp int, m Models) (out *Matrix) {
 	return
 }
 */
-func responseFn(which string) func(x float64, par ...float64) float64 {
-	fn := GaussSRF
-	switch which {
-	case "gauss": // Gaussian distribution
-		fn = GaussSRF
-	case "beta": // Generalised Beta of Austin, 2006
-		fn = GenBetaSRF
-	}
-	return fn
-}
-
-// Coenocline returns a matrix of species responses along (environmental) gradient. 
-func Coenocline(nSpec, nSamp int, m Models) (out *Matrix) {
-	var (
-		opt, tol, aMax float64
-	)
-
-	out = NewMatrix(nSamp, nSpec)
-
-	srf := responseFn(m.SRF)
-	points := generate_points(nSamp, m.Samp)    // generate sampling points
-	rngO := rndFn(m.Opt, m.OptMean, m.OptSigma) // optima distribution model
-
-	// if population size and tolerance models are both Gaussian, allow for covariance:  model is Multivariate Normal
-	mvNorm := false
-	if m.Pop == gaussian && m.Tol == gaussian {
-		mvNorm = true
-	}
-
-	rngA := rndFn(m.Pop, m.PopMean, m.PopSigma) // population size distribution model
-	rngT := rndFn(m.Tol, m.TolMean, m.TolSigma) // tolerance distribution model
-
-	// for every species: 
-	for j := 0; j < nSpec; j++ {
-		α, γ := 1.0, 1.0 // params of beta: to be reimplemented
-
-		// generate optimum (point on the gradient)
-		opt = rngO()
-
-		// generate species' population size and tolerance
-		if mvNorm { // population size and tolerance model is Multivariate Normal
-			// func Zeros(rows, cols int) *DenseMatrix
-			mu := mtx.Zeros(2, 1)
-			mu.Set(0, 0, m.PopMean)
-			mu.Set(1, 0, m.TolMean)
-			cov := mtx.Ones(2, 2)
-			cov.Set(0, 0, m.PopSigma*m.PopSigma) // needs justification
-			cov.Set(0, 1, m.PopTolRho*m.PopSigma*m.TolSigma)
-			cov.Set(1, 0, m.PopTolRho*m.PopSigma*m.TolSigma)
-			cov.Set(1, 1, m.TolSigma*m.TolSigma)
-			// func MVNormalNext(opt *DenseMatrix, Σ *DenseMatrix) *DenseMatrix
-			mvMat := MVNormalNext(mu, cov)
-			aMax = mvMat.Get(0, 0)
-			tol = mvMat.Get(1, 0)
-		} else {
-			aMax = rngA()
-			tol = rngT() // generate tolerance (range of acceptable gradient values)
-		}
-
-		// force max abundance and tolerance within some considerable limits
-		lo := m.PopMean - 3*m.PopSigma
-		if lo < 0 {
-			lo = 0
-		}
-		if aMax < lo {
-			aMax = lo
-		}
-		hi := m.PopMean + 3*m.PopSigma
-		if aMax > hi {
-			aMax = hi
-		}
-		lo = m.TolMean - 3*m.TolSigma
-		if lo < 0 {
-			lo = 0.1 * m.TolSigma
-		}
-		if tol < lo {
-			tol = lo
-		}
-		hi = m.TolMean + 3*m.TolSigma
-		if tol > hi {
-			tol = hi
-		}
-		for i := 0; i < nSamp; i++ {
-			x := points[i]
-			y := srf(x, opt, tol, α, γ)
-			// add "noise", if required
-			if m.Noise > 0 {
-				y = noise(y, m.Noise)
-			}
-			// scale by max abundance
-			y *= aMax
-			out.Set(i, j, y)
-		}
-	}
-	return
-}
--- eco/coenocline/coenocline.go
+++ eco/coenocline/coenocline.go
@@ -1,16 +1,14 @@
 // Coenocline modeller: Gaussian SRF (species population density response function). 
 // Models species population densities along (environmental) gradient sampled at distinct points. 
 // Density model is Gaussian (Normal) PDF, where, for every species,  μ is its optimum on the gradient, 
-// σ is a measure of tolerance,  99.7% of population lies within μ±3*σ. 
-// μ and σ themselves, and total population size have meta-models: uniform | Gaussian | Beta. 
-// Uniform and Beta distributions are reparametrized to use mean and standard deviation as parameters, for consistency.
+// ε is a measure of tolerance,  99.7% of population lies within μ±3*ε. 
+// μ and ε themselves, and total population size have meta-models: uniform | Gaussian | Beta. 
+// Uniform and Beta distributions are reparametrized to use mean and scale as parameters, for consistency.
 // Influences external to the model are modelled as Gaussian "noise".
 // Sample spacing model is uniform | exponential (Poisson process). 
 // Gradient spans from 0 to 1. 
 package main
 
-
-// To do: -p 2 does not work: panic: σ too big, α, β out of range
 
 import (
 	. "code.google.com/p/go-eco/eco/cc"
@@ -30,15 +28,20 @@ func main() {
 	sampModel := flag.Int("s", 0, "sampling model: 0 - regular, 1 - uniform, 2 - Poisson")
 	optModel := flag.Int("o", 0, "optima model: 0 - flat, 1 - Gaussian, 2 - Beta...")
 	μOpt := flag.Float64("om", 0.5, "optimum mean")
-	σOpt := flag.Float64("os", 0.35, "optimum standard deviation")
-	popModel := flag.Int("p", 0, "population size model: 0 - flat, 1 - Gaussian, 2 - Beta...")
-	μPop := flag.Float64("pm", 10.0, "mean population size")
-	σPop := flag.Float64("ps", 0.2, "standard deviation of population size, relative to population size = 1")
+	εOpt := flag.Float64("os", 0.35, "optimum scale")
+	denModel := flag.Int("p", 0, "model of maximum population densities: 0 - flat, 1 - Gaussian, ...")
+	μPop := flag.Float64("pm", 10.0, "mean maximum population density")
+	εPop := flag.Float64("ps", 0.2, "scale of maximum population density")
 	tolModel := flag.Int("t", 1, "tolerance model: 0 - flat, 1 - Gaussian, 2 - Beta...")
 	μTol := flag.Float64("tm", 0.2, "mean tolerance")
-	σTol := flag.Float64("ts", 0.4, "standard deviation of tolerance, relative to tolerance = 1")
+	εTol := flag.Float64("ts", 0.4, "scale of tolerance, relative to tolerance = 1")
 	ρPopTol := flag.Float64("r", 0.0, "correlation between ")
-	σNoise := flag.Float64("e", 0.0, "outer noise as standard deviation")
+	betaParamModel := flag.Int("beta", 1, "model for params of beta function")
+	μα := flag.Float64("am", 4.0, "mean of the alpha param to beta model")
+	εα := flag.Float64("as", 0.2, "scale of the alpha param to beta model")
+	μγ := flag.Float64("gm", 2.0, "mean gamma param to beta model")
+	εγ := flag.Float64("gs", 0.2, "scale of the gamma param to beta model")
+	εNoise := flag.Float64("e", 0.0, "outer noise as scale")
 	seed := flag.Int64("z", 1, "seed of random number generator")
 
 	flag.Parse()
@@ -51,7 +54,7 @@ func main() {
 
 	// set up models
 	mp:= &mod	// pointer to models
-	mp.SetUpModels(*srfModel, *sampModel, *optModel, *popModel, *tolModel, *μOpt, *σOpt, *μPop, *σPop, *μTol, *σTol, *ρPopTol, *σNoise)
+	mp.SetUpModels(*srfModel, *sampModel, *optModel, *denModel, *tolModel, *betaParamModel, *μOpt, *εOpt, *μPop, *εPop, *μTol, *εTol, *ρPopTol, *μα,  *εα, *μγ, *εγ,  *εNoise)
 
 	// compute the coenocline matrix
 	mtx := Coenocline(*nSpec, *nSamp, mod)
