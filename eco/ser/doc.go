b2317c2f3b32d1d5d5cc07b482a5c349c4b98ecd
Peter A. Cejchan <tyapca7@gmail.com>
2013-01-17 08:12:36 +0100

    some doc

--- eco/ser/doc.go
+++ eco/ser/doc.go
@@ -1,4 +1,4 @@
 // Copyright 2012 - 2013 The Eco Authors. All rights reserved. See the LICENSE file.
 
-// Package ser implements seriation.
+// Package ser implements seriation. Anti­Robinson simulated annealing, Boltzman MCMC methods.
 package ser
